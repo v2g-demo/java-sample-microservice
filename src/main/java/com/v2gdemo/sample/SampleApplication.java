d2ba92a0ae56118651b6494565c92d5e0cf16344
Andrey Bronin <andrey.bronin@insolar.io>
2019-03-17 21:18:19 +0300

    Spring boot 2 actuator

--- src/main/java/com/v2gdemo/sample/SampleApplication.java
+++ src/main/java/com/v2gdemo/sample/SampleApplication.java
@@ -1,13 +1,24 @@
 package com.v2gdemo.sample;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@RestController
 public class SampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
+		//SpringApplication.run(SampleApplication.class, args);
+		SpringApplication app = new SpringApplication(SampleApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
+
+
 
 }
